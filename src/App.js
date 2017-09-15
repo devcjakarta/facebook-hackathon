790ad3c3dd30bd1f5ce5a0ed7a884649ffbab550
Kevin Hermawan <kevinhermawanx@gmail.com>
2017-09-15 15:28:02 +0700

    Update fields

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
-import { Form, Button } from 'semantic-ui-react'
+import { Form, Button, Radio } from 'semantic-ui-react'
 import Navbar from './components/Navbar'
 import ContainerRegister from './components/ContainerRegister'
 import LabelRegister from './components/LabelRegister'
 import FormRegister from './components/FormRegister'
+import FormField from './components/FormField'
 
 class App extends Component {
   constructor() {
     super()
 
     this.state = {
-      firstName: '',
-      lastName: ''
+      fullName: '',
+      email: '',
+      phone: '',
+      hackTitle: '',
+      tech: ''
     }
     this.handleInputChange = this.handleInputChange.bind(this)
   }
@@ -37,17 +41,71 @@ class App extends Component {
               subTitle="Bring the world, closer together."
             />
             <FormRegister>
+              <FormField
+                label="Nama lengkap"
+                type="text"
+                name="fullName"
+                onChange={this.handleInputChange}
+                value={this.state.fullName}
+                placeholder="Masukan nama lengkap" />
+              
+              <FormField
+                label="E-mail"
+                type="email"
+                name="email"
+                onChange={this.handleInputChange}
+                value={this.state.email}
+                placeholder="Masukan alamat e-mail" />
+
+              <FormField
+                label="No. Telepon"
+                type="number"
+                name="phone"
+                onChange={this.handleInputChange}
+                value={this.state.phone}
+                placeholder="Masukan nomor telepon" />
+
+              <FormField
+                label="Judul Hackathon"
+                type="text"
+                name="hackTitle"
+                onChange={this.handleInputChange}
+                value={this.state.hackTitle}
+                placeholder="Masukan judul hackathon" />
+
               <Form.Field>
-               <label>First Name</label>
-               <input type="text" name="firstName" onChange={this.handleInputChange} value={this.state.firstName} placeholder='First Name' />
+                <label>Teknologi</label>
               </Form.Field>
               <Form.Field>
-                <label>Last Name</label>
-                <input type="text" name="lastName" onChange={this.handleInputChange} value={this.state.lastName} placeholder='Last Name' />
+                <Radio
+                  label='Facebook Stack'
+                  name='tech'
+                  value='Facebook Stack'
+                  checked={this.state.tech === 'Facebook Stack'}
+                  onChange={this.handleInputChange} />
               </Form.Field>
               <Form.Field>
-                <Button positive onClick={() => alert(`Name : ${this.state.firstName} ${this.state.lastName}`)}>Register</Button>
-                <Button color='facebook' onClick={() => alert(`Name : ${this.state.firstName} ${this.state.lastName}`)}>Register with Facebook</Button>
+                <Radio
+                  label='Umum'
+                  name='tech'
+                  value='Umum'
+                  checked={this.state.tech === 'Umum'}
+                  onChange={this.handleInputChange}
+                />
+              </Form.Field>
+              <Form.Field>
+                <Button positive onClick={() => alert(`
+                  Name : ${this.state.fullName}\n
+                  E-mail: ${this.state.email}\n
+                  No. Telp: ${this.state.phone}\n
+                  Title: ${this.state.hackTitle}\n
+                  Tech: ${this.state.tech}`)}>Register</Button>
+                <Button color="facebook" onClick={() => alert(`
+                  Name : ${this.state.fullName}\n
+                  E-mail: ${this.state.email}\n
+                  No. Telp: ${this.state.phone}\n
+                  Title: ${this.state.hackTitle}\n
+                  Tech: ${this.state.tech}`)}>Register with Facebook</Button>
               </Form.Field>
             </FormRegister>
           </ContainerRegister>
