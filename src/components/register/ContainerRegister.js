4d5ebf252cb645296fe762cbd0a9051d20c61cb2
Achmad Kurnianto <achmad.kurnianto@gmail.com>
2017-10-12 07:37:29 +0700

    transform ContainerRegister to stateless functional react component

--- src/components/register/ContainerRegister.js
+++ src/components/register/ContainerRegister.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { Container, Grid } from 'semantic-ui-react'
 
 import LabelRegister from './LabelRegister'
@@ -6,31 +6,18 @@ import FormRegister from './FormRegister'
 
 import {submitRegisterData} from '../../utils/register/methods'
 
-class ContainerRegister extends PureComponent {
-  submit = (data) => {
-    console.log(data)
-    return new Promise((resolve, reject) => {
-      API.post('/user', data)
-      .then(res => resolve(res))
-      .catch(err => {
-        console.log(JSON.stringify(err, null, 2))
-        reject(err)
-      })
-    })
-  }
-  render() {
-    return (
-      <Container>
-        <Grid>
-          <LabelRegister
-            title="Facebook Hackathon"
-            subTitle="Bring the world, closer together."
-          />
-          <FormRegister submit={this.submit} />
-        </Grid>
-      </Container>
-    )
-  }
+function ContainerRegister () {
+  return (
+    <Container>
+      <Grid>
+        <LabelRegister
+          title="Facebook Hackathon"
+          subTitle="Bring the world, closer together."
+        />
+        <FormRegister submit={submitRegisterData} />
+      </Grid>
+    </Container>
+  )
 }
 
 export default ContainerRegister
