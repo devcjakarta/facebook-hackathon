f1d9e14e4dadba30b12ce10a52c40703985735d3
Luri Darmawan <luri@kioss.com>
2017-10-13 17:27:39 +0700

    add sponsor logo

--- src/components/register/ContainerRegister.js
+++ src/components/register/ContainerRegister.js
@@ -13,6 +13,19 @@ function ContainerRegister () {
         <LabelRegister
           title="Jakarta Challenge 2017"
           subTitle="Bring the world, closer together."
+          organizerName="Hacktiv8"
+          organizerImg="http://jakarta-challenge.devcid.com/static/media/logo_hacktiv8.png"
+          sponsorGoldName="Facebook"
+          sponsorGoldImg="http://jakarta-challenge.devcid.com/static/media/logo-facebook.png"          
+          sponsorSilverImg="http://jakarta-challenge.devcid.com/static/media/logo-mainapi.png"
+          sponsorSilverName="MainAPI"
+          supportedBy1Name="BukaLapak"
+          supportedBy2Name="Kudo"
+          communityPartnerImg1="{image}"
+          communityPartnerImg2="{image}"
+          communityPartnerImg3="{image}"
+          communityPartnerImg4="{image}"
+          communityPartnerImg5="{image}"
         />
         <FormRegister submit={submitRegisterData} />
       </Grid>
--- src/components/register/LabelRegister.js
+++ src/components/register/LabelRegister.js
@@ -4,7 +4,7 @@ import { Grid, Header } from 'semantic-ui-react'
 
 import '../../styles/register/label-register.css'
 
-function LabelRegister ({ title, subTitle }) {
+function LabelRegister ({ title, subTitle, organizerName, organizerImg, sponsorGoldImg, sponsorSilverImg }) {
   return (
     <Grid.Column mobile={16} tablet={16} computer={8}>
       <div className="label-register">
@@ -14,6 +14,23 @@ function LabelRegister ({ title, subTitle }) {
         <Header as="h2">
           {subTitle}
         </Header>
+        <div style={{fontSize: 15, color: '#FFFFFF', marginTop: 60}}></div>
+        <div>
+          <div style={{float: 'left'}}>
+            <div style={{fontSize: 15}}>Organize By</div>
+            <img id="imgOrganizer" src={organizerImg} alt='{organizerName}' />
+          </div>
+          <div style={{float: 'left'}}>
+            <div style={{fontSize: 15}}>Gold Sponsor</div>
+            <img id="imgGoldSponsor" src={sponsorGoldImg} alt='Gold Sponsor' /> 
+          </div>
+          <div style={{float: 'left'}}>
+            <div style={{fontSize: 15}}>Silver Sponsor</div>
+            <img id="imgGoldSponsor" src={sponsorSilverImg} alt='Silver Sponsor' /> 
+          </div>
+        </div>
+        <div style={{fontSize: 15, clear: 'both', paddingTop: 10}}>Supported By</div>
+        <div style={{fontSize: 15, clear: 'both', paddingTop: 10}}>Community Partner</div>
       </div>
     </Grid.Column>
   )
@@ -21,7 +38,20 @@ function LabelRegister ({ title, subTitle }) {
 
 LabelRegister.propTypes = {
   title: PropTypes.string.isRequired,
-  subTitle: PropTypes.string.isRequired
+  subTitle: PropTypes.string.isRequired,
+  organizerName: PropTypes.string,
+  organizerImg: PropTypes.string,
+  sponsorGoldName: PropTypes.string,
+  sponsorGoldImg: PropTypes.string,
+  sponsorSilverImg: PropTypes.string,
+  sponsorSilverName: PropTypes.string,
+  supportedBy1Name: PropTypes.string,
+  supportedBy2Name: PropTypes.string,
+  communityPartnerImg1: PropTypes.string,
+  communityPartnerImg2: PropTypes.string,
+  communityPartnerImg3: PropTypes.string,
+  communityPartnerImg4: PropTypes.string,
+  communityPartnerImg5: PropTypes.string
 }
 
 export default LabelRegister
