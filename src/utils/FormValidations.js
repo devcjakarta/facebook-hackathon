936715d39050ae3d377e7362765b56a48952b0e6
Achmad Kurnianto <achmad.kurnianto@gmail.com>
2017-11-07 22:49:05 +0700

    edit comment in form validation

--- src/utils/FormValidations.js
+++ src/utils/FormValidations.js
@@ -1,7 +1,7 @@
 import isEmpty from 'validator/lib/isEmpty'
 import isEmail from 'validator/lib/isEmail'
 import isMobilePhone from 'validator/lib/isMobilePhone'
-import isURL from 'validator/lib/isURL'
+// import isURL from 'validator/lib/isURL'
 
 export function validateRegisterData (data) {
   let errors = {}
@@ -12,10 +12,8 @@ export function validateRegisterData (data) {
   if (!(isMobilePhone(data.phone, 'id-ID') && (data.phone.length > 9) && (data.phone.length < 14))) errors.phone = (errors.phone && errors.phone.length) ? `${errors.phone} dan valid` : "No telepon harus valid"
   if (isEmpty(data.title)) errors.title = "Judul hackathon harus diisi"
   if (isEmpty(data.description)) errors.description = "Deskripsi singkat harus diisi"
-  {/*
-  if (isEmpty(data.url)) errors.url = "URL harus diisi"
-  if (!isURL(data.url)) errors.url = (errors.url && errors.url.length) ? `${errors.url} dan valid` : "URL harus valid"
-*/}
+  // if (isEmpty(data.url)) errors.url = "URL harus diisi"
+  // if (!isURL(data.url)) errors.url = (errors.url && errors.url.length) ? `${errors.url} dan valid` : "URL harus valid"
   if (isEmpty(data.file)) errors.file = "Dokumen harus diunggah"
   return errors
 }
